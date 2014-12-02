05317f298c2a71c964c352862cf123a59c060d51
Babak Pourkazemi <bp5xj@virginia.edu>
2014-12-02 18:12:57 -0500

    RESTful routing, setup initial file structure, and all-in-all got express running.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tinker
+For hobbyists, by hobbyists.
+
+## What is it
+A platform for hobbyists to collaborate on ideas together. We bring the platform, you bring the skills. Written in Node.
--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
+var htmlDir = './public/';
 
-app.get('/', function(req, res) {
-    res.send('Hello world!');
+/****** Routing ******/
+app.all('*', function(req, res, next) {
+    console.log('\t| Request Url:', req.originalUrl);
+    console.log('\t| Request Type:', req.method);
+    next();
 });
 
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: htmlDir});
+});
+app.get('/projects', function(req, res) {
+    res.sendFile('projects.html', {root: htmlDir});
+});
+app.get('/users/:id', function(req, res, next) {
+    console.log('\t| ID:', req.params.id);
+    next();
+}, function(req, res) {
+    res.sendFile('user.html', {root: htmlDir});
+});
+app.get('/users/:id/config', function(req, res, next) {
+    console.log('\t| ID:', req.params.id);
+    next();
+}, function(req, res) {
+    res.sendFile('config.html', {root: htmlDir});
+});
+app.get('/users/:id/comments', function(req, res, next) {
+    console.log('\t| ID:', req.params.id);
+    next();
+}, function(req, res) {
+    res.sendFile('comments.html', {root: htmlDir});
+});
+
+/****** Settings  ******/
+
+/****** Middleware ******/
+app.use(express.static('public'));
+
+app.use(function(err, req, res, next) {
+    console.error( err.stack );
+    res.status(500).send("Something broke!");
+});
+
+/****** Server ******/
 var server = app.listen(3000, function() {
 
     var host = server.address().address;
